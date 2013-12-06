581d405ff46862571aab42b1eccd262a8e87f819
Matt Smith <matt@nearapogee.com>
2013-12-05 20:14:20 -0800

    add authentication.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Signed in"
+      redirect_to dashboard_url
+    else
+      flash.now[:notice] = "Email/password did not match."
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out!"
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Auth::Application.routes.draw do
-  get "sessions/new" # temporary
-  get "users/new" # temporary
+
+  resources :users, only: [:new, :create]
+  resource :session, only: %w(new create destroy)
 
   get '/dashboard', to: "dashboard#index"
   root "welcome#index"
